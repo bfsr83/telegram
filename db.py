b6ebc07afc57d062a8c82649e7c77ae5a2731d93
bfsr83 <60980923+bfsr83@users.noreply.github.com>
2020-02-12 18:58:51 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,26 +1,28 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 from config.auth import token
-if __name__ == '__main__':
-    updater = Updater(token=token)
-    dispatcher = updater.dispatcher
-    
+
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger('david_borges_bot')
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s -   
+                    %(message)s', level=logging.INFO)
+logger = logging.getLogger('david_borges')
 
-#present
+
 def start(bot, update):
     logger.info('He recibido un comando start')
     bot.send_message(
         chat_id=update.message.chat_id,
-        text="Soy un Achicayna, que entre los primeros pobladores de      
-             Canarias era el equivalente a un plebeyo."
+        text="Soy un Achicayna, que entre los primeros pobladores de 
+              Canarias era el equivalente a un plebeyo."
     )
 
-from telegram.ext import CommandHandler
 
-dispatcher.add_handler(CommandHandler('start', start))
+if __name__ == '__main__':
 
-updater.start_polling()
-updater.idle()
+    updater = Updater(token=token)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+
+    updater.start_polling()
+    updater.idle()
