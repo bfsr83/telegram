5ffcefdd9f39186dee3b445bac3c257f04b7ca56
bfsr83 <60980923+bfsr83@users.noreply.github.com>
2020-02-12 18:47:16 +0100

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-import telebot # Importamos las librería
-
-TOKEN = '992968153:AAHAMmgFVvJ5ARxKoY6Wwxdgp81bZiQbpQM' # Ponemos nuestro Token generado con el @BotFather
-
-tb = telebot.TeleBot(TOKEN) # Combinamos la declaración del Token con la función de la API
-
-# enviar mensagem simples
-
-tb.send_message(chatid, text) # Ejemplo tb.send_message('109556849', 'Hola mundo!')
