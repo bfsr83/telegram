2363e71f3bec591e503157c760c1637ad70f8fcc
bfsr83 <60980923+bfsr83@users.noreply.github.com>
2020-02-12 18:56:03 +0100

    Update db.py

--- db.py
+++ db.py
@@ -8,3 +8,12 @@ import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger('david_borges_bot')
+
+#present
+def start(bot, update):
+    logger.info('He recibido un comando start')
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Soy un Achicayna, que entre los primeros pobladores de      
+             Canarias era el equivalente a un plebeyo."
+    )
