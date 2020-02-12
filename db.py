21cf1c83357833d4e1d909671c38e465eaf0cc68
bfsr83 <60980923+bfsr83@users.noreply.github.com>
2020-02-12 18:53:59 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,10 @@
-teste
+from telegram.ext import Updater
+from config.auth import token
+if __name__ == '__main__':
+    updater = Updater(token=token)
+    dispatcher = updater.dispatcher
+    
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger('david_borges_bot')
