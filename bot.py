415b71e0ea4aafd0af331c5d0949159818dd2b07
bfsr83 <60980923+bfsr83@users.noreply.github.com>
2020-02-12 18:24:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
-print ("hello")
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('992968153:AAHAMmgFVvJ5ARxKoY6Wwxdgp81bZiQbpQM')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
